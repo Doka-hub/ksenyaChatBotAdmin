6081675c3eb3c58a9f579ec9ef72ffe9bdc5208b
Doka <dokadair@gmail.com>
2024-10-01 18:58:09 +0600

    update fields

--- apps/tgusers/serializers.py
+++ apps/tgusers/serializers.py
@@ -3,6 +3,23 @@ from .models import StartMessage
 
 
 class StartMessageSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField('get_photo_url')
+    video = serializers.SerializerMethodField('get_video_url')
+
     class Meta:
         model = StartMessage
         fields = ['id', 'text', 'photo', 'video']
+
+    def get_photo_url(self, obj):
+        if obj.photo:
+            photo_url = 'http://localhost:8001' + obj.photo.url
+        else:
+            photo_url = None
+        return photo_url
+
+    def get_video_url(self, obj):
+        if obj.video:
+            video_url = 'http://localhost:8001' + obj.video.url
+        else:
+            video_url = None
+        return video_url
