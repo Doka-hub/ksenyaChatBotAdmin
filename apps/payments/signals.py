405f8ff974a50ac1201491e0c551bf5bb6fd63cc
Doka <dokadair@gmail.com>
2024-10-02 19:19:46 +0600

    minor

--- apps/payments/signals.py
+++ apps/payments/signals.py
@@ -9,7 +9,7 @@ from .tasks import send_payment_request
 
 @receiver(post_save, sender=Payment)
 def payment_created(sender, instance, created, **kwargs):
-    if not created and instance.paid_at is None:
+    if not created and instance.paid_at is False:
         if instance.is_paid is True and instance.type == PaymentType.RB:
             send_payment_request.delay(instance.id)
             instance.paid_at = datetime.now ()
