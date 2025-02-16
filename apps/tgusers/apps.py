d4102ddf7d22f3cfcad537cbbcb127f6f8cc2a5a
Doka <dokadair@gmail.com>
2025-02-16 23:11:05 +0600

    добавить импорт сигналов в метод ready класса TgusersConfig[PS]

--- apps/tgusers/apps.py
+++ apps/tgusers/apps.py
@@ -5,3 +5,6 @@ class TgusersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tgusers'
     verbose_name = 'Телеграм Пользователи'
+
+    def ready(self):
+        from . import signals  # noqa
