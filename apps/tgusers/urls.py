7905b1e2bd43747b8c449957f4f37d5f52e9a0a4
Doka <dokadair@gmail.com>
2025-02-16 17:56:57 +0600

    изменить URL для представления StartMessageView: обновить путь с 'start-message/' на 'messages/'

--- apps/tgusers/urls.py
+++ apps/tgusers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import StartMessageView
 
 urlpatterns = [
-    path('start-message/', StartMessageView.as_view(), name='start_message_list_api'),
+    path('messages/', StartMessageView.as_view(), name='start_message_list_api'),
 ]
